a23979aed1d4160c6664a6ad3018c24d4833737c
Nagarani.R <nagaraniraja9677@gmail.com>
2017-11-29 10:30:12 +0530

    modified

--- pythonBranch/railRoads/railDataBase.py
+++ pythonBranch/railRoads/railDataBase.py
@@ -1,22 +1,27 @@
 import MySQLdb as mdb
 import datetime
-def trainDetailsFDatabase():
+
+    
+def boarding_description():
     con=mdb.connect(host='localhost',user='root',passwd="root",db="testdb")
     with con:
         cur=con.cursor()
         cur.execute("select DepatureTime,DepatureStation from TrainDetails")
         details=cur.fetchall()
         boardingDescription=[]
-        arrivalDescription=[]
-        Cities=[]
-    
         for detail in details:
             bordingtime =(datetime.datetime.min + detail[0]).time()
             boardDetails=[]
             boardDetails.append(str(bordingtime))
             boardDetails.append(detail[1])
-            boardingDescription.append(boardDetails)
+            boardingDescription.append(boardDetails)     
+    return boardingDescription        
 
+def arrival_description():
+    con=mdb.connect(host='localhost',user='root',passwd="root",db="testdb")
+    arrivalDescription=[]
+    with con:
+        cur=con.cursor()
         cur.execute("select ArrivalTime,ArrivalStation from TrainDetails")
         details=cur.fetchall()
         for detail in details:
@@ -24,9 +29,19 @@ def trainDetailsFDatabase():
             arrivalDetails=[]
             arrivalDetails.append(str(arrivaltime))
             arrivalDetails.append(detail[1])
-            arrivalDescription.append(arrivalDetails)
+            arrivalDescription.append(arrivalDetails)      
+    return arrivalDescription
+            
+def Cities():
+    con=mdb.connect(host='localhost',user='root',passwd="root",db="testdb")
+    Cities=[]
+    with con:
+        cur=con.cursor()
         cur.execute("select * from Cities")
         city=[item[0] for item in cur.fetchall()]
-        return boardingDescription,arrivalDescription,city
+        return city
 
-trainDetailsFDatabase()    
+boarding_description()
+arrival_description()
+Cities()
+  
