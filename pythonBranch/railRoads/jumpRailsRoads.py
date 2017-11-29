5618359ba7a99fb66cf9c665c182c1990aff62f2
Nagarani.R <nagaraniraja9677@gmail.com>
2017-11-29 10:29:15 +0530

    modified

--- pythonBranch/railRoads/jumpRailsRoads.py
+++ pythonBranch/railRoads/jumpRailsRoads.py
@@ -2,16 +2,21 @@ import railDataBase
 
 
 class Railroads:
-    bordinsDetails,arrivalDetails,Cities=railDataBase.trainDetailsFDatabase()
-    cities=Cities
-    def PointToPoint(self):
-        boardingStations=[]
-        arrivalStations=[]
+    def __init__(self):
+        self.bordinsDetails=railDataBase.boarding_description()
+        self.arrivalDetails=railDataBase.arrival_description()
+        self.Cities=railDataBase.Cities()
+        
+    def boarding_time(self):
         boardingTime=[]
         for i in range(0,len(self.bordinsDetails)):
             for j in range(0,len(self.bordinsDetails)):
                 boardingTime.append(self.bordinsDetails[i][j])
-                break
+                break   
+        return boardingTime    
+    def pointToPoint(self,boardingTime):
+        boardingStations=[]
+        arrivalStations=[]
         index=0
         for i in range(0,len(self.bordinsDetails)):
             for ind in range(0,len(boardingTime)):
@@ -23,35 +28,25 @@ class Railroads:
                                 arrivalStations.append(self.arrivalDetails[i])
                                 break
                     else:
-                        self.train_details(self.bordinsDetails[i],self.arrivalDetails[i])
+                        jumpBoardings.append(self.bordinsDetails[i])
+                        jumpDestinations.append(self.arrivalDetails[i])
                         break
                 else:
-                    self.train_details(self.bordinsDetails[i],self.arrivalDetails[i])
+                    jumpBoardings.append(self.bordinsDetails[i])
+                    jumpDestinations.append(self.arrivalDetails[i])
                     break
-        trainDescription=boardingStations+arrivalStations
         self.boardingdescription=boardingStations
         self.arivaldescription=arrivalStations
-        return trainDescription
-
-    def train_details(self,bordinsDetails,arrivalStations):
-        issuccess = True
-        try:
-            jumpBoardings.append(bordinsDetails)
-            jumpDestinations.append(arrivalStations)
-        except Exception as exception:
-            issuccess=False 
-            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-            message = template.format(type(exception).__name__, exception.args)
-        return issuccess
+        return boardingStations,arrivalStations,jumpBoardings,jumpDestinations
         
-    def jump_train(self):
+    def jump_train(self,jumpBoardings,jumpDestinations):
         boardstations=[]
         endstations=[]
         index=0
         for i in range(0,len(jumpBoardings)):
             if jumpBoardings[i][index+1]==self.boarding:
                 if jumpBoardings[i][index]>=self.starttime:
-                    if jumpDestinations[i][index+1] in self.cities:
+                    if jumpDestinations[i][index+1] in self.Cities:
                         if jumpDestinations[i][index]<=self.arrivaltime:
                             for j in range(0,len(jumpBoardings)):
                                 if jumpDestinations[i][index+1]==jumpBoardings[j][index+1]:
@@ -61,50 +56,64 @@ class Railroads:
                                                 boardstations.append(jumpBoardings[i])
                                                 endstations.append(jumpDestinations[j])
             else:
-                pass
+                pass    
         self.boarddescription=boardstations
         self.destinationdescription=endstations
-        jumpdescription=boardstations+endstations
-        return  jumpdescription   
+        return  boardstations,endstations
         
-    def  input_details(self):
+    def start_time(self):
         startTime=raw_input("Enter start time of jill:")
         self.starttime=startTime
+        return startTime
+    
+    def arrival_time(self):
         arrivalTime=raw_input("Correct time to arrival:")
         self.arrivaltime=arrivalTime
+        return arrivalTime
+    
+    def boarding_point(self):
         boardingPoint=raw_input("Enter boarding point:")
         self.boarding=boardingPoint
+        return boardingPoint
+    
+    def arrival_point(self):
         arrivalPoint=raw_input("Enter arrival point:")
         self.destination=arrivalPoint
+        return arrivalPoint
 
-    def pick_Train(self):
-       issuccess = True
-       try:
-           bording=self.boardingdescription+self.boarddescription
-           destination=self.arivaldescription+self.destinationdescription
-           index=0
-           time=[i[0] for i in bording]
-           Time=sorted(time, key=str, reverse=True)
-           for i in range(0,len(destination)):
-               if destination[i][index]==self.arrivaltime:
-                   print "the available train to go:",'\n',bording[i],'\n',destination[i]
-                   break
-               else:
-                   if destination[i][index]==Time[0]:
-                       print "available train to go:",'\n',bording[i],'\n',destination[i]
-                       break
-       except Exception as exception:
-           issuccess=False
-           template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-           message = template.format(type(exception).__name__, exception.args)
-       return issuccess
+    def pick_Train(self,boardingStations,arrivalStations,boardstations,endstations):
+        bording=boardingStations+boardstations
+        destination=arrivalStations+endstations
+        availabletrain=[]
+        index=0
+        time=[i[0] for i in bording]
+        Time=sorted(time, key=str, reverse=True)
+        for i in range(0,len(destination)):
+            if destination[i][index]==self.arrivaltime:
+                availabletrain.append(bording[i])
+                availabletrain.append(destination[i])
+                print "the available train to go:",'\n',bording[i],'\n',destination[i]
+                break
+            else:
+                if destination[i][index]==Time[0]:
+                    availabletrain.append(bording[i])
+                    availabletrain.append(destination[i])
+                    print "available train to go:",'\n',bording[i],'\n',destination[i]
+                    break         
+        return availabletrain
               
 if __name__ == "__main__":
     jumpBoardings=[]
     jumpDestinations=[]
     details=Railroads()
-    details.input_details()
-    details.PointToPoint()
-    details.jump_train()
-    details.pick_Train()
+    details.start_time()
+    details.arrival_time()
+    details.boarding_point()
+    details.arrival_point()
+    boardingTime=details.boarding_time()
+    boardingStations,arrivalStations,jumpBoardings,jumpDestinations=details.pointToPoint(boardingTime)
+    boardstations,endstations=details.jump_train(jumpBoardings,jumpDestinations)
+    details.pick_Train(boardingStations,arrivalStations,boardstations,endstations)
+    
+
     
